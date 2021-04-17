ca0e8c71c36c4e2ae46b23e91d9d5f9c84b1d612
mrtty35 <mrtty35@gmail.com>
2021-04-17 10:49:15 +0300

    Update restapirequestimpl.py

--- binance_f/impl/restapirequestimpl.py
+++ binance_f/impl/restapirequestimpl.py
@@ -217,15 +217,15 @@ class RestApiRequestImpl(object):
 
         request = self.__create_request_by_get("/fapi/v1/klines", builder)
 
-        def parse(json_wrapper):
-            result = list()
-            data_list = json_wrapper.convert_2_array()
-            for item in data_list.get_items():
-                element = Candlestick.json_parse(item)
-                result.append(element)
-            return result
+        #def parse(json_wrapper):
+        #    result = list()
+        #    data_list = json_wrapper.convert_2_array()
+        #    for item in data_list.get_items():
+        #        element = Candlestick.json_parse(item)
+        #        result.append(element)
+        #    return result
 
-        request.json_parser = parse
+        #request.json_parser = parse
         return request
 
     def get_mark_price(self, symbol):
